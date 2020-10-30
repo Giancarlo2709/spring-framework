e23a71965ef41cbb7e0a6f3f33d7cb23338705cd
Giancarlo <giancarlo2709@gmail.com>
2020-10-29 23:17:52 -0500

    add lifecyclebean

--- sfg-dependency-injection/src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ sfg-dependency-injection/src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -1,9 +1,6 @@
 package guru.springframework.sfgdi;
 
-import guru.springframework.sfgdi.controllers.ConstructorInjectedController;
-import guru.springframework.sfgdi.controllers.MyController;
-import guru.springframework.sfgdi.controllers.PropertyInjectedController;
-import guru.springframework.sfgdi.controllers.SetterInjectedController;
+import guru.springframework.sfgdi.controllers.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -16,6 +13,9 @@ public class SfgDiApplication {
 
   public static void main(String[] args) {
     ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
+
+    I18nController i18nController = ctx.getBean(I18nController.class);
+    System.out.println(i18nController.sayHello());
 
     MyController myController = ctx.getBean(MyController.class);
     /* String greeting = myController.sayHello();
