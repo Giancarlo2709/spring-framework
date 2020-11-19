1d2228ea59af452898719199bc317a82165a5872
Giancarlo <giancarlo2709@gmail.com>
2020-11-18 22:03:34 -0500

    add vets config

--- sfg-pet-clinic/pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
+++ sfg-pet-clinic/pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
@@ -1,6 +1,9 @@
 package guru.springframework.sfgpetclinic.controllers;
 
+import guru.springframework.sfgpetclinic.services.VetService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -10,9 +13,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class VetController {
 
-  @RequestMapping({"","/", "/index", "/index.html"})
-  public String listVets() {
+  private final VetService vetService;
 
+  @Autowired
+  public VetController(VetService vetService) {
+    this.vetService = vetService;
+  }
+
+  @RequestMapping({"","/", "/index", "/index.html"})
+  public String listVets(Model model) {
+    model.addAttribute("vets", this.vetService.findAll());
     return "vets/index";
   }
 }
