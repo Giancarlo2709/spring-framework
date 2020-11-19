84f04ca349e04b1c6ac66b9db77e96534058344b
Giancarlo <giancarlo2709@gmail.com>
2020-11-18 21:52:39 -0500

    list owners

--- sfg-pet-clinic/pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ sfg-pet-clinic/pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -34,7 +34,7 @@ public class DataLoader implements CommandLineRunner {
     ownerService.save(owner1);
 
     Owner owner2 = new Owner();
-    owner2.setId(1L);
+    owner2.setId(2L);
     owner2.setFirstName("Elvis");
     owner2.setLastName("Juarez");
 
