a9fd85007559793d23068149a75696fb98ac95f6
Giancarlo <giancarlo2709@gmail.com>
2020-11-20 21:41:19 -0500

    added PetTypeService

---
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,10 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.PetType;
+
+/**
+ * Interface <b>PetTypeService</b>.
+ * @author Giancarlo
+ */
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
