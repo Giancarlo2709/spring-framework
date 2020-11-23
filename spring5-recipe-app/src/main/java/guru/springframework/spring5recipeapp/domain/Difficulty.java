038d68c6a969e9585f7cf9fe057c74f3921238a1
Giancarlo <giancarlo2709@gmail.com>
2020-11-22 22:54:14 -0500

    adding many-to-many relationships

---
+++ spring5-recipe-app/src/main/java/guru/springframework/spring5recipeapp/domain/Difficulty.java
@@ -0,0 +1,10 @@
+package guru.springframework.spring5recipeapp.domain;
+
+/**
+ * Enum <b>Difficulty</b>.
+ * @author Giancarlo
+ */
+public enum Difficulty {
+
+  EASY, MODERATE, KIND_OF_HARD, HARD
+}
