3984b7050ea2301d26b600fcd5fbe09071a3eb83
Giancarlo <giancarlo2709@gmail.com>
2020-11-26 21:10:29 -0500

    Added visit entity

--- sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Visit.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Visit.java
@@ -1,5 +1,10 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import java.time.LocalDate;
 
 /**
@@ -7,10 +12,18 @@ import java.time.LocalDate;
  * <p>Entity Class Visit</p>
  * @author Giancarlo
  */
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity {
 
+  @Column(name = "date")
   private LocalDate date;
+
+  @Column(name = "description")
   private String description;
+
+  @ManyToOne
+  @JoinColumn(name = "pet_id")
   private Pet pet;
 
   public LocalDate getDate() {
