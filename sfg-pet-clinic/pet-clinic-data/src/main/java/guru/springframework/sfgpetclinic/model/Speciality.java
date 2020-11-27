8f968c11ebd8d6989b8915c39c9cdf926f74cc9c
Giancarlo <giancarlo2709@gmail.com>
2020-11-26 21:04:42 -0500

    updated vet to use JPA

--- sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Speciality.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Speciality.java
@@ -1,12 +1,19 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  * Class <b>Speciality</b>.
  * <p>Entity class Speciality</p>
  * @author Giancarlo
  */
+@Entity
+@Table(name = "specialties")
 public class Speciality  extends BaseEntity {
 
+  @Column(name = "description")
   private String description;
 
   public String getDescription() {
--- sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -1,5 +1,11 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -8,8 +14,13 @@ import java.util.Set;
  * <p>Entity Class Vet</p>
  * @author Giancarlo
  */
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
 
+  @ManyToMany(fetch = FetchType.EAGER)
+  @JoinTable(name = "vet_specialties", joinColumns = @JoinColumn(name = "vet_id"),
+          inverseJoinColumns = @JoinColumn(name = "speciality_id"))
   private Set<Speciality> specialities = new HashSet<>();
 
   public Set<Speciality> getSpecialities() {
