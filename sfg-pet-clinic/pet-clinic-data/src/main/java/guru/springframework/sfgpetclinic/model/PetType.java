bbd77d5375e9312e2c34f50a5163a09200de6512
Giancarlo <giancarlo2709@gmail.com>
2020-12-02 22:40:35 -0500

    find owners by id

--- sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -24,4 +25,9 @@ public class PetType extends BaseEntity {
 
   @Column(name = "name")
   private String name;
+
+  @Override
+  public String toString() {
+    return name;
+  }
 }
