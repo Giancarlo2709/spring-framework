c4679aeb21600d19a63d283cb61ed3a807c596ec
Giancarlo <giancarlo2709@gmail.com>
2020-12-04 23:05:34 -0500

    adding add and update pettype

--- sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -25,7 +25,6 @@ import java.util.Set;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 @Entity
 @Table(name = "pets")
 public class Pet extends BaseEntity {
@@ -46,4 +45,17 @@ public class Pet extends BaseEntity {
 
   @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
   private Set<Visit> visits = new HashSet<>();
+
+  @Builder
+  public Pet(Long id, String name, PetType petType, Owner owner, LocalDate birthDate, Set<Visit> visits) {
+    super(id);
+    this.name = name;
+    this.petType = petType;
+    this.owner = owner;
+    this.birthDate = birthDate;
+
+    if (visits == null || visits.size() > 0 ) {
+      this.visits = visits;
+    }
+  }
 }
